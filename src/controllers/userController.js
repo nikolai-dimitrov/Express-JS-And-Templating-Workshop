f25a1cc8cee22213206e3b685951d1c62280c760
nikolai-dimitrov <120419122+nikolai-dimitrov@users.noreply.github.com>
2023-10-04 16:06:00 +0300

    Initialized register post route

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -3,4 +3,11 @@ const router = require("express").Router();
 router.get("/register", (req, res) => {
   res.render("../views/user/register");
 });
+
+router.post("/register", (req, res) => {
+  const { username, password, repeatPassword } = req.body;
+
+  res.redirect('users/login');
+});
+
 module.exports = router;
