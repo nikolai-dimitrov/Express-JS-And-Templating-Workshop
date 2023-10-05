7f47048959a0b7cfb21a47c2fdf2c418e1e3e227
nikolai-dimitrov <120419122+nikolai-dimitrov@users.noreply.github.com>
2023-10-05 14:41:13 +0300

    Revert "Added login functionallity."
    This reverts commit bce51bb75a474f9e514b28d54d8f7289b4e36b35.

--- src/services/userServices.js
+++ src/services/userServices.js
@@ -1,10 +1,26 @@
 const User = require("../models/User");
+const bcrypt = require("bcrypt");
+const jwt = require("../lib/jwt");
+const { SECRET } = require("../constants");
 
 exports.register = async (userData) => {
   const newUser = await User.create(userData);
   return newUser;
 };
 
-// exports.getUsers = async () => {
-//   return User.userCollection();
-// };
+exports.login = async (username, password) => {
+  const user = await User.findOne({ username });
+  if (!user) {
+    throw new Error("Invalid username or password");
+  }
+  const passwordValid = await bcrypt.compare(password, user.password);
+  if (!passwordValid) {
+    throw new Error("Invalid username or password");
+  }
+  const payload = {
+    _id: username._id,
+    username: user.username,
+  };
+  const token = await jwt.sign(payload, SECRET, { expiresIn: "2d" });
+  return token;
+};
