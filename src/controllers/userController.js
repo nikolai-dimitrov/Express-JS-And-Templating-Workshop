bce51bb75a474f9e514b28d54d8f7289b4e36b35
nikolai-dimitrov <120419122+nikolai-dimitrov@users.noreply.github.com>
2023-10-05 14:39:55 +0300

    Added login functionallity.

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -1,24 +1,17 @@
 const router = require("express").Router();
 const userServices = require("../services/userServices");
-
 router.get("/register", (req, res) => {
   res.render("../views/user/register");
 });
 
 router.post("/register", async (req, res) => {
   const { username, password, repeatPassword } = req.body;
-  await userServices.register({ username, password, repeatPassword });
+  await userServices.register({username, password, repeatPassword});
   res.redirect("/users/login");
 });
 
 router.get("/login", async (req, res) => {
-  res.render("../views/user/login");
+    res.render("../views/user/login");
 });
 
-router.post("/login", async (req, res) => {
-  const { username, password } = req.body;
-  const token = await userServices.login(username, password);
-  res.cookie("auth", token, { httpOnly: true }); //{ httpOnly: true ,maxAge: 3600 }
-  res.redirect("/");
-});
 module.exports = router;
--- src/services/userServices.js
+++ src/services/userServices.js
@@ -1,26 +1,10 @@
 const User = require("../models/User");
-const bcrypt = require("bcrypt");
-const jwt = require("../lib/jwt");
-const { SECRET } = require("../constants");
 
 exports.register = async (userData) => {
   const newUser = await User.create(userData);
   return newUser;
 };
 
-exports.login = async (username, password) => {
-  const user = await User.findOne({ username });
-  if (!user) {
-    throw new Error("Invalid username or password");
-  }
-  const passwordValid = await bcrypt.compare(password, user.password);
-  if (!passwordValid) {
-    throw new Error("Invalid username or password");
-  }
-  const payload = {
-    _id: username._id,
-    username: user.username,
-  };
-  const token = await jwt.sign(payload, SECRET, { expiresIn: "2d" });
-  return token;
-};
+// exports.getUsers = async () => {
+//   return User.userCollection();
+// };
