58a155af08edc311b84498d1efd727ab65c2a293
nikolai-dimitrov <120419122+nikolai-dimitrov@users.noreply.github.com>
2023-10-06 13:33:00 +0300

    Added conditional navigation.

--- src/middlewares/authMiddleware.js
+++ src/middlewares/authMiddleware.js
@@ -6,8 +6,11 @@ exports.auth = async (req, res, next) => {
   if (token) {
     try {
       const user = await jwt.verify(token, SECRET);
-
+      //set user data into req (access user in all requests)
       req.user = user;
+      //set user and other variables into locals (easy accessible in all views .hbs)
+      res.locals.user = user;
+      res.locals.isAuthenticated = true;
 
       next();
     } catch (err) {
