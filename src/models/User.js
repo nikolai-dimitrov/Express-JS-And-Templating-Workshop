a0ca410f018fdacd14b4ca1c68f79bb4033bf627
nikolai-dimitrov <120419122+nikolai-dimitrov@users.noreply.github.com>
2023-10-07 16:11:23 +0300

    Added model validators.

--- src/models/User.js
+++ src/models/User.js
@@ -1,13 +1,37 @@
 const mongoose = require("mongoose");
 const bcrypt = require("bcrypt");
+const modelValidators = require("../validators/modelValidators");
 
 const userSchema = new mongoose.Schema({
   username: {
     type: "string",
+    minLength: [5, "Username should be at least 5 characters."],
+    validate: {
+      validator: function (value) {
+        return /^[A-Za-z0-9]+$/.test(value);
+      },
+      message: "Username must be letters and digits only.",
+      validator: async function (value) {
+        //Access model and find entity in validator.
+        // const user = mongoose.model("User").findOne({username: this.username})
+        return (await mongoose
+          .model("User")
+          .findOne({ username: this.username }))
+          ? false
+          : true;
+      },
+      message: "Username already exists.",
+    },
   },
-
   password: {
     type: "string",
+    minLength: [8, "Password must be at least 8 characters."],
+    validate: {
+      validator: function (value) {
+        return /^[A-Za-z0-9]+$/.test(value);
+      },
+      message: "Password must be letters and digits only.",
+    },
   },
 });
 // repeatPassword -> we pass to the model.create as extra parameter (we don't have it in to schema)
