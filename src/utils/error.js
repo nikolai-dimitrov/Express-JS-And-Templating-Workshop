53454d6a47a9c867158e410df0def7d0219f4da6
nikolai-dimitrov <120419122+nikolai-dimitrov@users.noreply.github.com>
2023-10-07 16:46:25 +0300

    Mapped errors into array.

--- src/utils/error.js
+++ src/utils/error.js
@@ -1,6 +1,9 @@
-exports.extractErrorMessage = function (error) {
-  if (error instanceof MongooseError) {
-    return Object.values(error.errors);
+const mongoose = require("mongoose");
+exports.extractErrorMessages = function (error) {
+  console.error(error);
+  if (error instanceof mongoose.MongooseError) {
+    // console.log(Object.values(error.errors).map((e) => e.message));
+    return Object.values(error.errors).map((e) => e.message);
   } else if (error instanceof Error) {
     return [error.message];
   }
